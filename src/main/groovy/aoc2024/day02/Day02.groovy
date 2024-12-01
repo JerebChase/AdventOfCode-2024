38023b5580700c986b43fee3263307a03e5590af
Jeremy Brown <chase208@4JLT.com>
2024-12-01 08:50:49 -0500

    Prepare for day 2

---
+++ src/main/groovy/aoc2024/day02/Day02.groovy
@@ -0,0 +1,19 @@
+package aoc2024.day02
+
+import aoc2024.common.Utils
+
+class Day02 {
+    static void main(String[] args) {
+        List<String> input = Utils.readFile("inputs/day02-input.txt")
+        println("The answer to part one is ${partOne(input)}")
+        println("The answer to part two is ${partTwo(input)}")
+    }
+
+    static partOne(List<String> input) {
+
+    }
+
+    static partTwo(List<String> input) {
+
+    }
+}
